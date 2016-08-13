ded98426ebe4c38d0fbb9cff10c18fe410ef996f
zylee <zylee005@163.com>
2016-08-13 19:17:27 +0800

    aa

---
+++ app/ctrl.js
@@ -0,0 +1,11 @@
+/**
+ * Created by zylee on 2016/8/6.
+ */
+import React from 'react';
+import ReactDom from 'react-dom';
+
+export default class  Main extends  Component {
+
+}
+
+ReactDom.render();
