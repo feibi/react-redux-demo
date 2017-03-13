7fefec2b75c4e73d312e86d798fc0c0fe989d7d4
lizhongyi <li.zhongyi@yujianjia.com>
2017-03-13 17:47:10 +0800

    add component Message

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -1,17 +1,63 @@
 import React from 'react'
 import {render} from 'react-dom'
 import QueueAnim from 'rc-queue-anim';
-
+import  Message from './../../components/Message'
 import Button from './../../components/button/button'
+
+const message = Message.newInstance({});
+
+
 class About extends React.Component {
     constructor(props) {
         super(props);
+        this.showMessage = this.showMessage.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.toMessage = this.toMessage.bind(this);
+        this.customClose = this.customClose.bind(this);
+    }
+
+    handleClick(e) {
+        message.create({
+            duration: 2,
+            content: <span>I'm a message</span>,
+            onClose() {
+
+            },
+        })
+    }
+
+    showMessage() {
+        message.create({
+            duration: null,
+            content: <span>I'm a closable message</span>,
+            closable: true,
+            onClose() {
+
+            },
+        })
+    }
+
+    toMessage() {
+        message.create({
+            duration: null,
+            key: 'sss',
+            content: <span>I'm a closable message<Button onClick={this.customClose.bind(null, 'sss')}/></span>,
+            onClose() {
+
+            },
+        })
+    }
+
+    customClose(key) {
+        message.removeMessage(key);
+    }
+
+    componentDidMount(){
 
     }
-    handleClick(e) {
-        // this.setState({
-        //     showSubComponent: !this.state.showSubComponent
-        // })
+
+    componentWillUnmount(){
+        //message.destroy()
     }
 
     render() {
@@ -27,7 +73,13 @@ class About extends React.Component {
 
                     abouts
                     <div className="panel">
-                        <Button onClick={this.handleClick.bind(this)}/>
+                        <Button title="默认自动关闭" onClick={this.handleClick}/>
+                    </div>
+                    <div className="panel">
+                        <Button title='默认手动关闭' onClick={this.showMessage}/>
+                    </div>
+                    <div className="panel">
+                        <Button title="自定义关闭" onClick={this.toMessage}/>
                     </div>
                 </div>
             </QueueAnim>
